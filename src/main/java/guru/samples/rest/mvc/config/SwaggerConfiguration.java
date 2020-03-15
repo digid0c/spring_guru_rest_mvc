6393ca57e92df64b07573fa23f05b62feeab4cbc
digid0c <digidoc22@gmail.com>
2020-03-15 19:10:28 +0200

    Customize Swagger configuration with API metadata

--- src/main/java/guru/samples/rest/mvc/config/SwaggerConfiguration.java
+++ src/main/java/guru/samples/rest/mvc/config/SwaggerConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
@@ -19,6 +23,22 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metadata());
+    }
+
+    private ApiInfo metadata() {
+        Contact contact = new Contact("Anonymous", "www.google.com", "anonymous@example.com");
+
+        return new ApiInfo(
+                "Spring MVC REST",
+                "Spring MVC REST application based on Spring Guru course",
+                "1.0",
+                "www.google.com",
+                contact,
+                "Some license",
+                "www.google.com",
+                new ArrayList<>()
+        );
     }
 }
