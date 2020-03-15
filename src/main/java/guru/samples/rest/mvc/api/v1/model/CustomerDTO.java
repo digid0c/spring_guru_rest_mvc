fe922a210a9112004d366de4604de1f94eeefd66
digid0c <digidoc22@gmail.com>
2020-03-15 20:12:00 +0200

    Customize customers API endpoints documentation

--- src/main/java/guru/samples/rest/mvc/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/samples/rest/mvc/api/v1/model/CustomerDTO.java
@@ -1,5 +1,7 @@
 package guru.samples.rest.mvc.api.v1.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +11,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "This model represents Customer entity")
 public class CustomerDTO {
 
+    @ApiModelProperty(value = "Customer ID")
     private Long id;
+
+    @ApiModelProperty(value = "Customer first name")
     private String firstName;
+
+    @ApiModelProperty(value = "Customer last name")
     private String lastName;
 }
--- src/main/java/guru/samples/rest/mvc/controller/v1/CustomerController.java
+++ src/main/java/guru/samples/rest/mvc/controller/v1/CustomerController.java
@@ -3,6 +3,8 @@ package guru.samples.rest.mvc.controller.v1;
 import guru.samples.rest.mvc.api.v1.model.CustomerDTO;
 import guru.samples.rest.mvc.api.v1.model.CustomerListDTO;
 import guru.samples.rest.mvc.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import static guru.samples.rest.mvc.controller.v1.CustomerController.BASE_URL;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
+@Api(value = "This API allows to perform CRUD operations with customers")
 @RestController
 @RequestMapping(BASE_URL)
 public class CustomerController {
@@ -23,36 +26,42 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "Get a list of all customers")
     @GetMapping
     @ResponseStatus(OK)
     public CustomerListDTO findAll() {
         return new CustomerListDTO(customerService.findAll());
     }
 
+    @ApiOperation(value = "Find a customer with specified ID", notes = "Throws ResourceNotFoundException")
     @GetMapping("/{id}")
     @ResponseStatus(OK)
     public CustomerDTO findById(@PathVariable Long id) {
         return customerService.findById(id);
     }
 
+    @ApiOperation(value = "Create a new customer")
     @PostMapping
     @ResponseStatus(CREATED)
     public CustomerDTO create(@RequestBody CustomerDTO customerDTO) {
         return customerService.create(customerDTO);
     }
 
+    @ApiOperation(value = "Full update of an existing customer details", notes = "Throws ResourceNotFoundException")
     @PutMapping("/{id}")
     @ResponseStatus(OK)
     public CustomerDTO update(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return customerService.update(id, customerDTO);
     }
 
+    @ApiOperation(value = "Patch an existing customer specified properties", notes = "Throws ResourceNotFoundException")
     @PatchMapping("/{id}")
     @ResponseStatus(OK)
     public CustomerDTO patch(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return customerService.patch(id, customerDTO);
     }
 
+    @ApiOperation(value = "Delete a customer")
     @DeleteMapping("/{id}")
     @ResponseStatus(OK)
     public void delete(@PathVariable Long id) {
